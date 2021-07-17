b0b29af4e3c1e59c71bb250025f4964805e524e7
tanushree <tanu283shree@gmail.com>
2021-07-17 23:25:55 +0530

    Ankit Bday

--- app/routes.py
+++ app/routes.py
@@ -4,24 +4,23 @@ from flask import render_template, request,url_for,jsonify
 @app.route('/')
 def home():
 	questions=[]
-	f = list(open("app/q.txt", "r"))
-	h = list(open("app/hash_Tags.txt", "r"))
-	for x in range(20):
+	f = list(open("app/kk_ques.txt", "r"))
+	h = list(open("app/kk_hash_tags.txt", "r"))
+	for x in range(10):
 		question={}
 		question["no."]=x+1
 		question["hashtag"]=h[x].strip("\n")
-		question["statement"]=f[x*4+0].strip("\n")[2:]
-		question["options"]=f[x*4+1].strip("\n")
-		question["answer"]=f[x*4+2].strip("\n")[2:]
+		question["statement"]=f[x*3+0].strip("\n")[2:]
+		question["options"]=f[x*3+1].strip("\n")
 		questions.append(question)
 	return render_template('home.html',questions=questions)
 
 
 @app.route('/get_result/<string:ans_id>',methods=['GET'])
 def get_result(ans_id):
-	h = list(open("app/hash_Tags.txt", "r"))
+	h = list(open("app/kk_hash_tags.txt", "r"))
 	id=int(ans_id.split("_")[1])
-	hashtag=h[id+19]
+	hashtag=h[id+9]
 	with open("app/"+ans_id+".txt",'r',encoding="utf8") as f:
 		result=f.read()
 	type,content=result.split("|")
